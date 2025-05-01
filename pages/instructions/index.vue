84748de183fd8553ea33a2494e0ad711858b20b6
vovisim_home <wova53209@yandex.ru>
2025-05-01 11:12:19 +0300

    начал делать поиск

---
+++ pages/instructions/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+const search: Object = ref("");
+
+</script>
+
+<template>
+<div class="pt-12">
+  <div class="lg:mx12">
+    <v-text-field
+      v-model="search"
+      label="Search"
+      prepend-inner-icon="mdi-magnify"
+      variant="outlined"
+      hide-details="auto"
+      single-line
+      rounded="lg"
+    />
+  </div>
+
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>

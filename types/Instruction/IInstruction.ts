5e909a539d2b9f6f14176aab055330cb7be591de
vovisim_home <wova53209@yandex.ru>
2025-05-02 19:05:05 +0300

    поправил ошибки

--- types/Instruction/IInstruction.ts
+++ types/Instruction/IInstruction.ts
@@ -1,8 +1,10 @@
+import type { ISubinstruction } from "~/types/Subinstruction/ISubinstruction";
+
 export interface IInstruction {
   id: InstructionId;
   title: string;
   logo: string;
   short_description: string;
   description: string;
-  subinstructions: Object[];
+  subinstructions: ISubinstruction[];
 }
--- types/Instruction/type.d.ts
+++ types/Instruction/type.d.ts
@@ -12,16 +12,28 @@ export interface IInstructionPayloadCreate extends IInstructionPayloadBase {
   short_description: string;
   description: string;
 }
+
 export interface IInstructionResponseCreate extends IInstructionResponseBase {}
+
+export interface IInstructionPayloadGet extends IInstructionPayloadBase {}
 
 export interface IInstructionResponseGet extends IPagination<IInstructionResponseBase> {}
 
+export interface IInstructionPayloadUpdate extends IInstructionPayloadBase {
+  title?: string;
+  logo?: string;
+  short_description?: string;
+  description?: string;
+}
+
 export interface IInstructionResponseUpdate extends IInstructionResponseBase {}
+
+export interface IInstructionPayloadSearch extends IInstructionPayloadBase {}
 
 export interface IInstructionResponseSearch extends IPagination<IInstructionResponseBase> {}
 
-export interface IInstructionPayloadDelete extends IInstructionPayloadBase {
+export interface IInstructionPayloadDelete extends IInstructionPayloadBase {}
+
+export interface IInstructionResponseDelete extends IInstructionResponseBase {
   message: string
 }
-
-export interface IInstructionResponseDelete extends IInstructionResponseBase {}

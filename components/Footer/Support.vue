3aec485b91bab0cfed58ebf2400dce26484cf989
vovisim_laptop <wova53209@yandex.ru>
2025-05-09 19:30:55 +0300

    TFRONTEND 1 | Сделал базовые иконки

--- components/Footer/Support.vue
+++ components/Footer/Support.vue
@@ -1,4 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const socialLinks = computed(() => ({
+  vk: process.env.VK_COMMUNITY || "https://vk.com",
+  reddit: process.env.REDDIT_COMMUNITY || "https://reddit.com",
+  telegram: process.env.TELEGRAM_COMMUNITY || "https://telegram.org",
+}));
+</script>
 
 <template>
   <div
@@ -7,7 +13,17 @@
     <a class="underline hover:text-blue-400 transition-all" href="">
       Отправить запрос в поддержку
     </a>
-    <div>сотсети</div>
+    <div class="flex gap-2 w-full justify-center">
+      <a :href="socialLinks.vk">
+        <IconsVk height="1.25rem" width="1.25rem" />
+      </a>
+      <a :href="socialLinks.reddit">
+        <IconsReddit height="1.25rem" width="1.25rem" />
+      </a>
+      <a :href="socialLinks.telegram">
+        <IconsTelegram height="1.25rem" width="1.25rem" />
+      </a>
+    </div>
     <div>
       © 2025
       <a href="" class="underline hover:text-blue-400 transition-all">
